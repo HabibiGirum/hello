ab356dbebf7b3ef377cd3cdd62492c6a973e0ab6
Habtamu <habtamugirum0489@gmail.com>
2024-01-13 22:42:27 +0300

    unp

--- macOs/platform/macos/productbuild.cmake
+++ macOs/platform/macos/productbuild.cmake
@@ -1,12 +1,12 @@
 
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
-set(CPACK_COMMAND_PRODUCTBUILD "${CMAKE_CURRENT_SOURCE_DIR}/macOs/platform/macos/macpack/control/pkg/productbuild.sh")
+set(CPACK_COMMAND_PRODUCTBUILD "macOs/platform/macos/macpack/control/pkg/productbuild.sh")
 set(CPACK_COMMAND_PKGBUILD "${CPACK_COMMAND_PRODUCTBUILD}")
 
 install(
   FILES
-    "${CMAKE_CURRENT_SOURCE_DIR}/macOs/platform/macos/macpack/control/pkg/io.osquery.agent.conf"
-    "${CMAKE_CURRENT_SOURCE_DIR}/macOs/platform/macos/macpack/control/pkg/io.osquery.agent.plist"
+    "macOs/platform/macos/macpack/control/pkg/io.osquery.agent.conf"
+    "macOs/platform/macos/macpack/control/pkg/io.osquery.agent.plist"
 
   DESTINATION
     "/private/var/osquery"
