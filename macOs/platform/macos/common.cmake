ffba76f5c7c440634a614a72803a4283f68edd0d
Habtamu <habtamugirum0489@gmail.com>
2024-01-13 22:37:49 +0300

    new

--- macOs/platform/macos/common.cmake
+++ macOs/platform/macos/common.cmake
@@ -1,11 +1,4 @@
-#
-# Copyright (c) 2014-present, The osquery authors
-#
-# This source code is licensed as defined by the LICENSE file found in the
-# root directory of this source tree.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
-#
+
 
 set(OSQUERY_PACKAGE_RELEASE "1.macos")
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}_${OSQUERY_PACKAGE_RELEASE}_${CMAKE_SYSTEM_PROCESSOR}")
--- macOs/platform/macos/productbuild.cmake
+++ macOs/platform/macos/productbuild.cmake
@@ -1,11 +1,3 @@
-#
-# Copyright (c) 2014-present, The osquery authors
-#
-# This source code is licensed as defined by the LICENSE file found in the
-# root directory of this source tree.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
-#
 
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 set(CPACK_COMMAND_PRODUCTBUILD "${CMAKE_CURRENT_SOURCE_DIR}/macOs/platform/macos/macpack/control/pkg/productbuild.sh")
@@ -13,8 +5,8 @@ set(CPACK_COMMAND_PKGBUILD "${CPACK_COMMAND_PRODUCTBUILD}")
 
 install(
   FILES
-    "macOs/platform/macos/macpack/control/pkg/io.osquery.agent.conf"
-    "macOs/platform/macos/macpack/control/pkg/io.osquery.agent.plist"
+    "${CMAKE_CURRENT_SOURCE_DIR}/macOs/platform/macos/macpack/control/pkg/io.osquery.agent.conf"
+    "${CMAKE_CURRENT_SOURCE_DIR}/macOs/platform/macos/macpack/control/pkg/io.osquery.agent.plist"
 
   DESTINATION
     "/private/var/osquery"
